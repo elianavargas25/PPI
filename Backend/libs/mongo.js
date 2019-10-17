ad90362ba386550588c3047a238cf36c6bf31298
eliana.vargas <eliana.vargas@quipux.com>
2019-10-17 13:37:30 -0500

    Se crea modifica services

--- Backend/libs/mongo.js
+++ Backend/libs/mongo.js
@@ -1,37 +1,75 @@
-const {MongoClient, ObjectId} = require('mongodb');
+const { MongoClient, ObjectId } = require('mongodb');
+const { config } = require('../config');
 
-const MONGO_URI = 'AQUI_TU_URI_MONGO_ATLAS';
+const USER = encodeURIComponent(config.dbUser);
+const PASSWORD = encodeURIComponent(config.dbPassword);
+const DB_NAME = config.dbName;
+
+const MONGO_URI = `mongodb+srv://${USER}:${PASSWORD}@${config.dbHost}/${DB_NAME}?retryWrites=true&w=majority`;
 
 class MongoLib {
-    constructor(){
-        this.client = new MongoClient(MONGO_URI, {useNewUrlParser : true});
-        this.dbName = 'db_finanzas';
+  constructor() {
+    this.client = new MongoClient(MONGO_URI, { useNewUrlParser: true });
+    this.dbName = DB_NAME;
+  }
+
+  connect() {
+    if (!MongoLib.connection) {
+      MongoLib.connection = new Promise((resolve, reject) => {
+        this.client.connect(err => {
+          if (err) {
+            reject(err);
+          }
+
+          console.log('Connected succesfully to mongo');
+          resolve(this.client.db(this.dbName));
+        });
+      });
     }
 
-    connect(){
-        if(!MongoLib.connection){
-            MongoLib.connection = new Promise((resolve, reject) => {
-                this.client.connect(err => {
-                    if(err){
-                        reject(err)
-                    }
+    return MongoLib.connection;
+  }
 
-                    resolve(this.client.db(this.dbName));
-                })
-            })
-        }
+  getAll(collection, query) {
+    return this.connect().then(db => {
+      return db
+        .collection(collection)
+        .find(query)
+        .toArray();
+    });
+  }
 
-        return MongoLib.connection;
-    }
+  get(collection, id) {
+    return this.connect().then(db => {
+      return db.collection(collection).findOne({ _id: ObjectId(id) });
+    });
+  }
 
-    getAll(collection, query){
-        return this.connect().then(db => {
-            return db
-            .collection(collection)
-            .find(query)
-            .toArray();
-        })
-    }
+  create(collection, data) {
+    return this.connect()
+      .then(db => {
+        return db.collection(collection).insertOne(data);
+      })
+      .then(result => result.insertedId);
+  }
+
+  update(collection, id, data) {
+    return this.connect()
+      .then(db => {
+        return db
+          .collection(collection)
+          .updateOne({ _id: ObjectId(id) }, { $set: data }, { upsert: true });
+      })
+      .then(result => result.upsertedId || id);
+  }
+
+  delete(collection, id) {
+    return this.connect()
+      .then(db => {
+        return db.collection(collection).deleteOne({ _id: ObjectId(id) });
+      })
+      .then(() => id);
+  }
 }
 
 module.exports = MongoLib;
--- Backend/services/users.js
+++ Backend/services/users.js
@@ -8,10 +8,29 @@ class UsersService  {
 
     }
 
-    async getusers ( { tags }){
+    async getUsers ( { tags }){
         const query = tags && {$in : {tags}}  
         const users = await this.mongoDB.getAll(this.collection,query)
         return users || []
+    }
+    async getUser( { movieId }){
+        const user = await this.mongoDB.get(this.collection, userId);
+        return user || {};
+    }
+
+    async createUser( { user }){
+        const createdUserId = await this.mongoDB.create(this.collection, user);
+        return createdUserId;
+    }
+
+    async udpateUser({ userId, user } = { }){
+        const updatedUserId = await this.mongoDB.update(this.collection, userId, user);
+        return updatedUserId;
+    }
+
+    async deleteUser({ userId }){
+        const deletedUserId = await this.mongoDB.delete(this.collection, userId);
+        return deletedUserId;
     }
 
 }
