fc351aeb391ef9273d7de7999c6d90cc8018250b
mileidyramos <mileidy_ramos23171@gmail.com>
2019-10-19 13:46:10 -0500

    Se crea ruta de ingreso

--- Backend/config/index.js
+++ Backend/config/index.js
@@ -4,10 +4,10 @@ const config ={
     dev : process.env.NODE_ENV !== "production",
     port : process.env.PORT || 3000,
     cors : process.env.CORS,
-    dbUser : process.env.DB_USER || admin,
-    dbPassword : process.env.DB_PASSWORD || admin,
+    dbUser : process.env.DB_USER || "admin",
+    dbPassword : process.env.DB_PASSWORD || "admin",
     dbHost : process.env.DB_HOST,
-    dbName : process.env.DB_NAME || FINANZAS
+    dbName : process.env.DB_NAME || "FINANZAS"
 }
 
 module.exports = {config};
