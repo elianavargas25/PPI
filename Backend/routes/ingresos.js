7197802e6a7e4930bf4947e81b4cd382e9c84f64
mileidyramos <mileidy_ramos23171@gmail.com>
2019-10-20 17:00:44 -0500

    Se eliminan espacios del archivo

--- Backend/routes/ingresos.js
+++ Backend/routes/ingresos.js
@@ -10,10 +10,10 @@ function ingresosApi(app) {
     const ingresosService = new IngresosService();
 
     //Obteniendo toda la lista de usuarios
-    router.get("/", async function (req, res, next){
+    router.get("/", async function (req, res, next) {
         //los tags vienen del query de la url
         const { tags } = req.query;
-        try{
+        try {
             //filtramos las usuarios que queremos ver por unos tags
             const ingresos = await ingresosService.getIngresos({ tags });
 
@@ -21,67 +21,67 @@ function ingresosApi(app) {
                 data: ingresos,
                 message: 'ingresos listed'
             })
-        }catch(err){
+        } catch (err) {
             next(err);
         }
     });
     //Obteniendo un usuario en particular
-    router.get("/:ingresoId", async function (req, res, next){
+    router.get("/:ingresoId", async function (req, res, next) {
         //en este caso el id viene como parámetro en la URL
         const { ingresoId } = req.params;
-        try{
-            const ingreso = await ingresosService.getIngreso( { ingresoId });
+        try {
+            const ingreso = await ingresosService.getIngreso({ ingresoId });
 
             res.status(200).json({
                 data: ingreso,
                 message: 'ingreso retrieved'
             })
-        }catch(err){
+        } catch (err) {
             next(err);
         }
     });
     //Creando las usuarios
-    router.post("/", async function (req, res, next){
+    router.post("/", async function (req, res, next) {
         //sacamos del cuerpo (body) de la petición la película.
-        const { body : ingreso } = req;
-        try{
-            const createdIngresoId = await ingresosService.createIngreso( { ingreso});
+        const { body: ingreso } = req;
+        try {
+            const createdIngresoId = await ingresosService.createIngreso({ ingreso });
 
             res.status(201).json({
                 data: createdIngresoId,
                 message: 'ingresos created'
             })
-        }catch(err){
+        } catch (err) {
             next(err);
         }
     });
     //Actualizando una película
-    router.put("/:ingresoId", async function (req, res, next){
+    router.put("/:ingresoId", async function (req, res, next) {
         //con el put recibo dos aspectos, el cuerpo y el parametro (id de ingreso a actualizar)
-        const { body : ingreso } = req;
+        const { body: ingreso } = req;
         const { ingresoId } = req.params;
 
-        try{
-            const updatedIngresoId = await ingresosService.udpateIngreso({ ingresoId , ingreso });
+        try {
+            const updatedIngresoId = await ingresosService.udpateIngreso({ ingresoId, ingreso });
             res.status(200).json({
                 data: updatedIngresoId,
                 message: 'ingreso updated'
             })
-        }catch(err){
+        } catch (err) {
             next(err);
         }
     });
     //Eliminar usuarios
-    router.delete("/:ingresoId", async function (req, res, next){
+    router.delete("/:ingresoId", async function (req, res, next) {
         const { ingresoId } = req.params;
-        try{
+        try {
             const deletedIngresoId = await ingresosService.deleteIngreso({ ingresoId });
 
             res.status(200).json({
                 data: deletedIngresoId,
                 message: 'ingreso deleted'
             })
-        }catch(err){
+        } catch (err) {
             next(err);
         }
     });
