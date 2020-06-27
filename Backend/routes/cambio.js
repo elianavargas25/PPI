0e7c0ec345d16f8a54391f716c260f16e87723d6
santiago966 <santyboal2@gmail.com>
2020-06-27 13:42:26 -0500

    Cambio contra

--- Backend/routes/cambio.js
+++ Backend/routes/cambio.js
@@ -1,42 +1,21 @@
 const express = require('express');
 //importando nuestros servicios
-const updatePass = require('../services/users');
+const updatePass = require('../services/cambio');
 const { verificaToken } = require('../middlewares/authentication');
 
 function cambioContra(app) {
     const router = express.Router();
     app.use("/api/cambio", router);
-
-    //instanciando un nuevo servicio
-    const updatePas = new updatePass();
-    router.get("/", verificaToken, async function(req, res, next) {
-        //en este caso el id viene como parámetro en la URL
-        return res.json({
-            user: req.usuario
-        });
-        const { userId } = req.params;
-        try {
-            const user = await updatePas.getUser({ userId });
-
-            res.status(200).json({
-                data: user,
-                message: 'user retrieved'
-            })
-        } catch (err) {
-            next(err);
-        }
-    });
-
-    router.put("/contra", verificaToken, async function(req, res, next) {
-        //con el put recibo dos aspectos, el cuerpo y el parametro (id de ingreso a actualizar)
+    const updateCon = new updatePass();
+    router.put("/:userId", verificaToken, async function(req, res, next) {
         const { body: user } = req;
         const { userId } = req.params;
         try {
-            const updateContra = await updatePas.udpateUser({ userId, user });
+            const usuario = await updateCon.updateContra({ userId, user });
             res.status(200).json({
-                data: updateContra,
-                message: 'Contra updated'
-            })
+                Usuario: usuario,
+                message: 'Usuario actualizado'
+            });
         } catch (err) {
             next(err);
         }
--- Backend/routes/sesion.js
+++ Backend/routes/sesion.js
@@ -11,16 +11,16 @@ function sesionApi(app) {
     //instanciando un nuevo servicio
     const inicioService = new InicioService();
 
-    router.post("/", async function(req, res, next) {
+    router.get("/", async function(req, res, next) {
         //los tags vienen del query de la url
         const tags = req.query;
         try {
             //filtramos las usuarios que queremos ver por unos tags
             const users = await inicioService.getInicio(tags);
+            let token = jwt.sign({
+                usuario: users
+            }, config.seed, { expiresIn: 60 * 60 * 24 * 1 });
             if (users["_id"]) {
-                let token = jwt.sign({
-                    usuario: users
-                }, config.seed, { expiresIn: 60 * 60 * 24 * 1 });
                 res.status(200).json({
                     data: users,
                     message: 'user alredy exist',
